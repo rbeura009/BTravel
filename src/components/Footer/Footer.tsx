f501f21e8556c3bcc1d9f7f9c46159ab91556884
Rashmiranjan <siburashmiranjan1@gmail.com>
2021-11-24 15:51:09 +0530

    project setup

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import classes from './Footer.module.css';
+
+export default function Footer() {
+    return (
+        <div>
+            Footer
+        </div>
+    )
+}
